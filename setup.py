60d5775b150a787d9da7295a59edcabcec0d61f6
Ilmar Hurkxkens <hurkxkens@arch.ethz.ch>
2019-04-23 12:58:56 +0200

    update setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='depthscanner',
+      version='0.1',
+      description='Returns a pointcloud from the 3D scanner',
+      url='https://github.com/ilmihur/depthscanner',
+      author='Ilmar Hurkxkens',
+      author_email='hurkxkens@arch.ethz.ch',
+      license='MIT',
+      packages=['depthscanner'],
+      zip_safe=False)
