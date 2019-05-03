49ecce5d8c364d4c433110864d2281d28c1480e2
ilmihur <hurkxkens@arch.ethz.ch>
2019-05-03 17:14:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='depthscanner',
       version='0.1',
-      description='Returns a pointcloud from the 3D scanner',
+      description='Imports 3D points from an Intel Realsense Depth Camera into Rhino Grasshopper',
       url='https://github.com/ilmihur/depthscanner',
       author='Ilmar Hurkxkens',
       author_email='hurkxkens@arch.ethz.ch',
